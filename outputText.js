4ed6be031a13db1ccb32775842eb39e0356586f3
nilsberge <nilsberge2@gmail.com>
2016-10-11 19:19:43 +1100

    Update outputText.js
    Enabled unique element ID to target correct textarea

--- outputText.js
+++ outputText.js
@@ -3,11 +3,12 @@ function outputText(txt) {
     var doc = document;
     var name = 'outputTextArea';
     var count = doc.getElementsByClassName(name).length + 1;
+    var elemId = name + count;
     var outputEl = doc.createElement('textarea');
     outputEl = doc.getElementsByTagName('body')[0].appendChild(outputEl);
-    outputEl.id = name + count;
+    outputEl.id = elemId;
     outputEl.className = name;
-    outputEl.style.cssText = 'color:black;display:block;font-size:12px;font-family:"Courier New",Arial,Helvetica;border:1px solid black;position:fixed;top:' + (count * 5) + 'px;left:' + (count * 5) + 'px;z-index:2147483647;width:600px;height:400px;padding:6px 10px;';
-    outputEl.setAttribute('onkeyup', 'if(event.keyCode==27){var el=document.getElementById(this.id);el.parentNode.removeChild(el);}');
+    outputEl.style.cssText = 'color:black;display:block;font-size:12px;font-family:"Courier New",Arial,Helvetica;border:1px solid black;position:fixed;top:' + (count * 5) + 'px;left:' + (count * 5) + 'px;z-index:2147483647;width:640px;height:480px;padding:6px 10px;';
+    outputEl.setAttribute('onkeyup', 'if(event.keyCode==27){var el=document.getElementById("' + elemId + '");el.parentNode.removeChild(el);}');
     outputEl.value = txt;
 }
